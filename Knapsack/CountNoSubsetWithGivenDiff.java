0df1789aa514ce129aa509c3078a7dc11cd15ee4
Avishkar Sabale <avisabale7230@gmail.com>
2025-10-02 19:36:16 +0530

    count Subset with given Diff

---
+++ Knapsack/CountNoSubsetWithGivenDiff.java
@@ -0,0 +1,107 @@
+import java.util.Arrays;
+
+public class CountNoSubsetWithGivenDiff {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 2, 3};
+        int d = 1;
+
+        System.out.println("Array: " + Arrays.toString(arr));
+        System.out.println("Difference: " + d);
+        System.out.println("-----------------------------------");
+        System.out.println("1. Result (Memoization): " + countPartitionsMemoization(arr, d));
+        System.out.println("2. Result (Tabulation): " + countPartitionsTabulation(arr, d));
+        System.out.println("3. Result (Space Optimized): " + countPartitionsOptimized(arr, d));
+    }
+
+    public static int countPartitionsMemoization(int[] arr, int d) {
+        int n = arr.length;
+        int totalSum = 0;
+        for (int num : arr) {
+            totalSum += num;
+        }
+
+        if (totalSum - d < 0 || (totalSum - d) % 2 != 0) {
+            return 0;
+        }
+
+        int target = (totalSum - d) / 2;
+        int[][] dp = new int[n][target + 1];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+
+        return solveMemo(arr, target, n - 1, dp);
+    }
+
+    private static int solveMemo(int[] arr, int sum, int index, int[][] dp) {
+        if (index < 0) {
+            return (sum == 0) ? 1 : 0;
+        }
+
+        if (dp[index][sum] != -1) {
+            return dp[index][sum];
+        }
+
+        int exclude = solveMemo(arr, sum, index - 1, dp);
+        int include = 0;
+        if (arr[index] <= sum) {
+            include = solveMemo(arr, sum - arr[index], index - 1, dp);
+        }
+
+        return dp[index][sum] = include + exclude;
+    }
+
+    public static int countPartitionsTabulation(int[] arr, int d) {
+        int n = arr.length;
+        int totalSum = 0;
+        for (int num : arr) {
+            totalSum += num;
+        }
+
+        if (totalSum - d < 0 || (totalSum - d) % 2 != 0) {
+            return 0;
+        }
+
+        int target = (totalSum - d) / 2;
+        int[][] dp = new int[n + 1][target + 1];
+        dp[0][0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= target; j++) {
+                int exclude = dp[i - 1][j];
+                int include = 0;
+                if (arr[i - 1] <= j) {
+                    include = dp[i - 1][j - arr[i - 1]];
+                }
+                dp[i][j] = include + exclude;
+            }
+        }
+
+        return dp[n][target];
+    }
+
+    public static int countPartitionsOptimized(int[] arr, int d) {
+        int n = arr.length;
+        int totalSum = 0;
+        for (int num : arr) {
+            totalSum += num;
+        }
+
+        if (totalSum - d < 0 || (totalSum - d) % 2 != 0) {
+            return 0;
+        }
+
+        int target = (totalSum - d) / 2;
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+
+        for (int num : arr) {
+            for (int j = target; j >= num; j--) {
+                dp[j] = dp[j] + dp[j - num];
+            }
+        }
+
+        return dp[target];
+    }
+}
