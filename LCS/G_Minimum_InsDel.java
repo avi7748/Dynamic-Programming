d70aceb2a46ce185fa0dff43a3b99ffbf11fbe5d
Avishkar Sabale <avisabale7230@gmail.com>
2025-10-17 21:32:14 +0530

    13.0 Commit

---
+++ LCS/G_Minimum_InsDel.java
@@ -0,0 +1,31 @@
+package LCS;
+
+public class G_Minimum_InsDel {
+
+    private static int LCS(String X, String Y, int m, int n, Integer[][] dp) {
+        if(m == 0 || n == 0) return 0;
+
+        if(dp[m][n] != null){
+            return dp[m][n];
+        }
+
+        if(X.charAt(m-1) == Y.charAt(n-1)) {
+            return dp[m][n] = 1 + LCS(X,Y,m-1,n-1,dp);
+        }
+        else {
+            return dp[m][n] = Math.max(LCS(X,Y,m,n-1,dp), LCS(X,Y,m-1,n,dp));
+        }
+    }
+
+    public static void main(String[] args) {
+        String X = "ABCD";
+        String Y = "ABX  ";
+        int m = X.length();
+        int n = Y.length();
+
+        Integer[][] dp = new Integer[m+1][n+1];
+
+        System.out.println(m + n - 2 * LCS(X,Y,m,n,dp));
+    }
+
+}
